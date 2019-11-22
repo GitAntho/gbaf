17b897fd33f3eef099a0faae90392a7b37dd3ab5
Anthony <anthonydr@free.fr>
2019-11-22 14:36:12 +0100

    Maj de la db

--- Classes/Database.php
+++ Classes/Database.php
@@ -7,7 +7,12 @@ class Database
     {
         try
         {
-            $db = new PDO('mysql:host=localhost;dbname=gbaf', 'root', '');
+            $db_name = "gbaf";
+            $db_user = "root";
+            $db_pass = "";
+            $db_host = "localhost";
+
+            $db = new PDO('mysql:host=' . $db_host .';dbname=' . $db_name, $db_user, $db_pass);
             $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         }
         catch (Exception $e)
