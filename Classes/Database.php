446528dd971ca06642e55f5070791c145e415d7a
Anthony <anthonydr@free.fr>
2020-01-25 18:01:04 +0100

    maj des noms

--- Classes/Database.php
+++ Classes/Database.php
@@ -7,10 +7,10 @@ class Database
     {
         try
         {
-            $db_host = "localhost";
-            $db_name = "gbaf";
-            $db_user = "root";
-            $db_pass = "";
+            $db_host = "host";
+            $db_name = "name";
+            $db_user = "user";
+            $db_pass = "password";
 
             $db = new PDO('mysql:host=' . $db_host .';dbname=' . $db_name, $db_user, $db_pass);
             $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
